d978adb686d591fbb2ec4a9071b53e25f28b2f5b
Patrice Cote <patrice.cote@ia.ca>
2021-01-03 15:00:09 -0500

    Added Chapter8 samples

---
+++ Chapitre8/Indexes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Indexes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Samantha Jones";
+            int lengthOfFirst = name.IndexOf(' '); 
+            int lengthOfLast = name.Length - lengthOfFirst - 1;
+
+            ReadOnlySpan<char> nameAsSpan = name.AsSpan(); 
+            var firstNameSpan = nameAsSpan[0..lengthOfFirst]; 
+            var lastNameSpan = nameAsSpan[^lengthOfLast..^0]; 
+            Console.WriteLine($" First name: {firstNameSpan.ToString()}, Last name: {lastNameSpan.ToString()}");
+        }
+    }
+}
